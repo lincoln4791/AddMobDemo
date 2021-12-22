673a6f0496fd5e9bf5896db3a1dbacc291646360
lincoln4791 <mahmuduulkarim@gmail.com>
2021-12-22 12:13:35 +0600

    Native Ad Placed2

--- app/src/main/java/com/example/addmobdemo/NativeAd2.kt
+++ app/src/main/java/com/example/addmobdemo/NativeAd2.kt
@@ -1,11 +1,22 @@
 package com.example.addmobdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.ads.nativetemplates.TemplateView
 
 class NativeAd2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_native_ad2)
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            startActivity(Intent(this,NativeAddSmall::class.java))
+            finish()
+        }
+
+        Util.loadNativeAd(this,findViewById<TemplateView>(R.id.my_template))
+
     }
 }
